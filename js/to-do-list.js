110fd63de5a9dfe361083bd50dc5a6300a83141c
Flaviu Ratiu <ratiu.flaviu@gmail.com>
2019-11-11 20:18:30 +0200

    displaying items from api in web page

--- js/to-do-list.js
+++ js/to-do-list.js
@@ -9,7 +9,35 @@ window.ToDoList = {
         }).done(function (response) {
             console.log("GET done");
             console.log(response);
+
+            ToDoList.displayItems(JSON.parse(response));
         });
+    },
+
+    displayItems: function (items) {
+        var tableContent = '';
+
+        items.forEach(item => tableContent += ToDoList.getItemTableRow(item));
+
+        $("#to-do-items tbody").html(tableContent);
+    },
+
+    getItemTableRow: function (item) {
+        // spread (...)
+        var deadline = new Date(...item.deadline).toLocaleDateString("ro");
+
+        // ternary operator
+        var checkedAttribute = item.done ? "checked" : "";
+
+        return `<tr>
+            <td>${item.description}</td>
+            <td>${deadline}</td>
+            <td><input type="checkbox" class="mark-done" 
+            data-id="${item.id}" ${checkedAttribute}/></td>
+            <td><a href="#" class="delete-item" data-id="${item.id}">
+                <i class="far fa-trash-alt"></i>
+            </a></td>
+        </tr>`
     }
 
 };
